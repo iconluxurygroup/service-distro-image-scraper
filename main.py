6676fa097a47388c9e6a612cac9351e236a59510
nikwif <149531476+nikwif@users.noreply.github.com>
2025-02-24 17:58:09 +0000

    boto3 s3

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def get_spaces_client():
     #                         region_name=os.getenv('REGION'),
     #                         aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
     #                         aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
-    client = boto3.client(service_name='s3',
+    client = boto3.client('s3',
                                region_name=os.getenv('REGION'),
                              aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
                              aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
