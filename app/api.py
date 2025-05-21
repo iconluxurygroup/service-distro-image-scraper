7a660bcc490c3538ac33350ecd86ed2d336b74fa
Nik Popov <149531476+nikwif@users.noreply.github.com>
2025-05-21 17:27:07 +0000

    batch 10 rabbiut

--- app/api.py
+++ app/api.py
@@ -41,7 +41,7 @@ from url_extract import extract_thumbnail_url
 from openpyxl import Workbook
 from openpyxl.styles import PatternFill
 from openpyxl.utils import get_column_letter
-from rabbitmq_producer import RabbitMQProducer
+from rabbitmq_consumer import RabbitMQConsumer
 app = FastAPI(title="super_scraper", version=VERSION)
 
 default_logger = logging.getLogger(__name__)
