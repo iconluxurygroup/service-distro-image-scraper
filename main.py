5b2d6c374967f129ddb5f1c10c2c1e1148679c25
Nik Popov <149531476+nikwif@users.noreply.github.com>
2025-05-22 22:27:08 +0000

    core

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ from sqlalchemy import create_engine
 import pyodbc
 
 import pandas as pd
-from config import engine, conn_str
+from app_config import engine, conn_str
 import base64,zlib
 from s3_utils import upload_file_to_space
 from email_utils import send_message_email,send_email
--- s3_utils.py
+++ s3_utils.py
@@ -10,9 +10,9 @@ from tenacity import retry, stop_after_attempt, wait_exponential, retry_if_excep
 from sqlalchemy.exc import SQLAlchemyError
 import aiobotocore.session
 from aiobotocore.config import AioConfig
-from config import S3_CONFIG
+from app_config import S3_CONFIG
 from sqlalchemy.sql import text
-from config import async_engine, conn_str, engine
+from app_config import async_engine, conn_str, engine
 import pyodbc
 
 default_logger = logging.getLogger(__name__)
