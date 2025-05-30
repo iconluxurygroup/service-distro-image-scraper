03a803029a4e7bb6397f1fe253f4db2524a7fb08
Nik Popov <149531476+nikwif@users.noreply.github.com>
2025-05-30 22:05:35 +0000

    core ; git

--- app/rabbitmq_consumer.py
+++ app/rabbitmq_consumer.py
@@ -364,6 +364,7 @@ async def main():
 if __name__ == "__main__":
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
+    consumer = RabbitMQConsumer
     try:
         signal.signal(signal.SIGINT, signal_handler(consumer, loop))
         signal.signal(signal.SIGTERM, signal_handler(consumer, loop))
