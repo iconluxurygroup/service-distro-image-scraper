01f3b784792450c6237f589529dcb0d60ef05c77
Nik Popov <149531476+nikwif@users.noreply.github.com>
2025-05-30 19:42:34 +0000

    core ; git

--- app/rabbitmq_test.py
+++ app/rabbitmq_test.py
@@ -8,7 +8,7 @@ from rabbitmq_producer import RabbitMQProducer, get_producer
 from rabbitmq_consumer import RabbitMQConsumer
 from config import RABBITMQ_URL
 from tenacity import retry, stop_after_attempt, wait_fixed, retry_if_exception_type
-import aiormq
+import aiormq,datetime
 
 logger = logging.getLogger(__name__)
 if not logger.handlers:
