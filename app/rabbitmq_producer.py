9a4f8fc13f76e46706d20d155880aadda20aead5
Nik Popov <149531476+nikwif@users.noreply.github.com>
2025-05-29 20:16:52 -0400

    core

--- app/rabbitmq_producer.py
+++ app/rabbitmq_producer.py
@@ -7,7 +7,7 @@ from fastapi import BackgroundTasks
 from tenacity import retry, stop_after_attempt, wait_exponential, retry_if_exception_type
 import asyncio
 import aiormq.exceptions
-
+import uuid
 logger = logging.getLogger(__name__)
 
 class RabbitMQProducer:
